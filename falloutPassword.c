adb10b221404c7ca7a0ccbae2b0685da55bee1db
RodolphoVSantoro <rodolphovs@gmail.com>
2024-04-28 00:56:40 -0300

    minor refactor

--- falloutPassword.c
+++ falloutPassword.c
@@ -2,106 +2,122 @@
 #include <stdlib.h>
 #include <string.h>
 
-void liberaVetor(char **, int);
-void imprimeVetor(char **, int);
-void troca(char **, char **);
-int findMaiorSimilaridade(int *, int);
+// free the memory allocated for the string array and its strings
+void freeStringArray(char **, int);
+// prints the strings in the string array
+void printStringArray(char **, int);
+// pass &s1 and &s2 to swap the strings
+void swap(char **, char **);
+// gets passwords from file and sets n to how many strings were read
+char **getPasswordsFromFile(const char *, int *);
+// gets similarity between two strings
+int getMaxSimilarity(int *, int);
 // similaridade entre duas strings
-int similaridade(char *, char *);
+int getSimilarity(char *, char *);
 // similaridade de uma possível senha com as outras
 int similaridadeAcumulada(char **, int, int);
-char **getPasswordsFromFile(const char *, int *);
+// remove strings that don't have the same similarity as the attempt from the array of possible passwords
+void filterBySimilarity(char *, char **, int *, int);
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
+        printf("Error, Usage: %s <filename>\n", argv[0]);
         exit(1);
     }
     char *fname = argv[1];
 
-    int passwordsLength, possibleCorrect, attemptAmount = 0, sim = 0, indRecomendado;
+    int passwordsLength;
 
     // gets all passwords from file and converts to array of strings
     // sets n to how many strings were read
     char **passwords = getPasswordsFromFile(fname, &passwordsLength);
 
     // creates a list of similarities between each strings
-    int *similaridadeAcumuladas = (int *)malloc(sizeof(int) * passwordsLength);
+    int *accumulatedSimilarityAmount = (int *)malloc(sizeof(int) * passwordsLength);
 
     // saves each string that was attempted on the terminal
-    char tentativas[4][256];
+    char attempt[4][256];
 
-    possibleCorrect = passwordsLength;
-    while (possibleCorrect > 0 && attemptAmount < 4) {
+    int possibleCorrectAmount = passwordsLength;
+    int attemptAmount = 0;
+    while (possibleCorrectAmount > 1 && attemptAmount < 4) {
         // reprocess the similarities between the strings that could still be passwords
         // because array changes after each attempt
-        for (int i = 0; i < possibleCorrect; i++) {
-            similaridadeAcumuladas[i] = similaridadeAcumulada(passwords, i, possibleCorrect);
+        for (int i = 0; i < possibleCorrectAmount; i++) {
+            accumulatedSimilarityAmount[i] = similaridadeAcumulada(passwords, i, possibleCorrectAmount);
         }
 
         // gets the index of the string with the highest similarity
-        indRecomendado = findMaiorSimilaridade(similaridadeAcumuladas, possibleCorrect);
+        int recommendedIndex = getMaxSimilarity(accumulatedSimilarityAmount, possibleCorrectAmount);
+
+        int similarityFromGame;
 
         // prints the possibilities with a recommendation
-        printf("Possibilidades tentativa %d:\n", attemptAmount);
-        imprimeVetor(passwords, possibleCorrect);
-        printf("\nRecomendado tentar %s\n", passwords[indRecomendado]);
+        printf("Attempt number %d:\n", attemptAmount);
+        printStringArray(passwords, possibleCorrectAmount);
+        printf("\nTry %s\n", passwords[recommendedIndex]);
         // asks for a new attempt
-        printf("Tente uma palavra\n");
-        scanf("%s", tentativas[attemptAmount]);
+        printf("Write down the attempt\n");
+        scanf("%s", attempt[attemptAmount]);
         // asks its similarity given by the game
-        printf("Quantas iguais a senha?\n");
-        scanf("%d", &sim);
-        if (sim == strlen(tentativas[attemptAmount])) {
-            printf("A senha eh %s(len)\n", tentativas[attemptAmount]);
+        printf("How similar did the game say the attempt was?\n");
+        scanf("%d", &similarityFromGame);
+        if (similarityFromGame == strlen(attempt[attemptAmount])) {
+            printf("The password is %s\n", attempt[attemptAmount]);
             break;
         }
+
         // remove strings that don't have the same similarity as the attempt from the array of possible passwords
-        for (int i = 0; i < possibleCorrect; i++) {
-            int similarityToAttempt = similaridade(tentativas[attemptAmount], passwords[i]);
-            if (similarityToAttempt != sim) {
-                printf("i=%d\n", i);
-                troca(&passwords[i], &passwords[possibleCorrect - 1]);
-                possibleCorrect--;
-                i--;
-            }
-        }
-        if (possibleCorrect == 1) {
-            printf("A senha eh %s\n", passwords[0]);
-            break;
-        }
-        if (possibleCorrect == 0) {
-            printf("Algo deu errado\n");
-            break;
-        }
-        printf("ending loop t=%d, possibleCorrect=%d\n", attemptAmount, possibleCorrect);
+        filterBySimilarity(attempt[attemptAmount], passwords, &possibleCorrectAmount, similarityFromGame);
+
         attemptAmount++;
     }
-    printf("k=%d, t=%d\n", possibleCorrect, attemptAmount);
-    free(similaridadeAcumuladas);
-    liberaVetor(passwords, passwordsLength);
+
+    if (possibleCorrectAmount != 1) {
+        printf("Something went wrong\n");
+        free(accumulatedSimilarityAmount);
+        freeStringArray(passwords, passwordsLength);
+        return 1;
+    }
+
+    printf("The password is %s\n", passwords[0]);
+    free(accumulatedSimilarityAmount);
+    freeStringArray(passwords, passwordsLength);
     return 0;
 }
 
-int similaridade(char *s1, char *s2) {
-    int i, s = 0;
-    for (i = 0; s1[i] != 0 && s2[i] != 0; i++) {
-        if (s1[i] == s2[i]) {
-            s++;
+void filterBySimilarity(char *attempt, char **passwords, int *possibleCorrectAmount, int similarityFromGame) {
+    for (int i = 0; i < *possibleCorrectAmount; i++) {
+        int similarityToAttempt = getSimilarity(attempt, passwords[i]);
+        if (similarityToAttempt != similarityFromGame) {
+            swap(&passwords[i], &passwords[*possibleCorrectAmount - 1]);
+            (*possibleCorrectAmount)--;
+            i--;
         }
     }
-    return s;
 }
 
-int similaridadeAcumulada(char **strs, int ind, int tam) {
-    int i, SA = 0;
-    for (i = 0; i < tam; i++)
-        if (i != ind)
-            SA += similaridade(strs[i], strs[ind]);
-    return SA;
+int getSimilarity(char *s1, char *s2) {
+    int similarity = 0;
+    for (int i = 0; s1[i] != '\0' && s2[i] != '\0'; i++) {
+        if (s1[i] == s2[i]) {
+            similarity++;
+        }
+    }
+    return similarity;
 }
 
-void troca(char **s1, char **s2) {
+int similaridadeAcumulada(char **strs, int indexCompared, int tam) {
+    int accumulatedSimilarity = 0;
+    for (int i = 0; i < tam; i++) {
+        if (i != indexCompared) {
+            accumulatedSimilarity += getSimilarity(strs[i], strs[indexCompared]);
+        }
+    }
+    return accumulatedSimilarity;
+}
+
+void swap(char **s1, char **s2) {
     char *str = *s1;
     *s1 = *s2;
     *s2 = str;
@@ -131,29 +147,37 @@ char **getPasswordsFromFile(const char *fname, int *n) {
         fscanf(file, "%s", passwords[i]);
     }
 
+    char len = strlen(passwords[0]);
+    for (int i = 0; i < *n; i++) {
+        if (strlen(passwords[i]) != len) {
+            printf("Error: all passwords must have the same length\n");
+            exit(1);
+        }
+    }
+
     fclose(file);
     return passwords;
 }
 
-int findMaiorSimilaridade(int *v, int tam) {
-    int i, indMaior = 0;
-    for (i = 0; i < tam; i++)
-        if (v[i] > v[indMaior])
-            indMaior = i;
-    return indMaior;
+int getMaxSimilarity(int *array, int size) {
+    int i, indexMaxSimilarity = 0;
+    for (i = 0; i < size; i++) {
+        if (array[i] > array[indexMaxSimilarity]) {
+            indexMaxSimilarity = i;
+        }
+    }
+    return indexMaxSimilarity;
 }
 
-void liberaVetor(char **strs, int size) {
-    int i;
-    for (i = 0; i < size; i++) {
+void freeStringArray(char **strs, int size) {
+    for (int i = 0; i < size; i++) {
         free(strs[i]);
     }
     free(strs);
 }
 
-void imprimeVetor(char **strs, int size) {
-    int i;
-    for (i = 0; i < size; i++) {
+void printStringArray(char **strs, int size) {
+    for (int i = 0; i < size; i++) {
         printf("%s\n", strs[i]);
     }
 }
